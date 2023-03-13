7908ab0ffedd9070cf3f367fd465ef63d85fe99c
rudraramya <ramyarudra104@outlook.com>
2023-03-14 00:41:52 +0530

    changed host file

--- infra/resource.tf
+++ infra/resource.tf
@@ -78,7 +78,7 @@ connection {
     type     = "ssh"
     user     = "rudraramya"
     password = "Ramya$123456"
-    host     = element(azurerm_linux_virtual_machine.vm.*.public_ip, 0)
+    host     = element(azurerm_linux_virtual_machine.vm.*.azurerm_public_ip.ip, 0)
   }
 provisioner "file" {
     source      = "/home/rudraramya/common/workspace/spc/target/spring-petclinic-3.0.0-SNAPSHOT.jar"
